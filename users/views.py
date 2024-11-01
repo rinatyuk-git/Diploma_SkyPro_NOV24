8997db39004b955d6a52bca4c92a73e9c861cdc5
rinatyuk-git <17_ufo_RB@proton.me>
2024-11-02 00:25:12 +0100

    create User views

---
+++ users/views.py
@@ -0,0 +1,41 @@
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+    """ Creation of User """
+    serializer_class = UserSerializer
+    queryset = User.objects
+    permission_classes = [AllowAny]
+
+    def perform_create(self, serializer):
+        """ Owner Override """
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
+
+
+class UserListAPIView(generics.ListAPIView):
+    """ Viewing List of Users """
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserRetrieveAPIView(generics.RetrieveAPIView):
+    """ Viewing Details of User """
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+    """ User Update """
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserDestroyAPIView(generics.DestroyAPIView):
+    """ Deleting User """
+    queryset = User.objects.all()
