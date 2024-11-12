1bc7549a4f03cb06102cc4dc79cc9e0e78954b97
rinatyuk-git <17_ufo_RB@proton.me>
2024-11-12 18:31:32 +0100

    create mailing regard approved/declined docs

--- content/admin.py
+++ content/admin.py
@@ -1,5 +1,29 @@
 from django.contrib import admin
 
 from content.models import Document
+from content.tasks import doc_approving_message
 
-admin.site.register(Document)
+
+@admin.action(description="Mark selected docs as approved")
+def make_approved(modeladmin, request, queryset):
+    queryset.update(status="a")
+    """ определение отправки сообщения """
+    for obj in queryset:
+        doc_approving_message.delay(obj.id)
+
+
+@admin.action(description="Mark selected docs as declined")
+def make_declined(modeladmin, request, queryset):
+    queryset.update(status="d")
+    """ определение отправки сообщения """
+    for obj in queryset:
+        doc_approving_message.delay(obj.id)
+
+
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ["name", "status"]
+    ordering = ["name"]
+    actions = [make_approved, make_declined]
+
+
+admin.site.register(Document, DocumentAdmin)
