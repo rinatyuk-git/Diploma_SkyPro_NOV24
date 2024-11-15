b42ea0a36d36c3dcb36555dbacdcf4152e26c528
rinatyuk-git <17_ufo_RB@proton.me>
2024-11-15 21:01:21 +0100

    cleanup code under flake hints

--- content/tasks.py
+++ content/tasks.py
@@ -7,7 +7,7 @@ from users.models import User
 
 
 def doc_created_message(mailing_item: Document):
-    """ Подготовка рассылки писем """
+    """ Preparing of mailing """
     send_mail(
         f'Doc {mailing_item.name} created',
         f'Doc {mailing_item.name} created info',
@@ -19,11 +19,12 @@ def doc_created_message(mailing_item: Document):
 
 @shared_task
 def doc_approving_message(instance_id: int):
-    """ Подготовка рассылки писем для подтверждения """
+    """ Preparing of mailing regard to approve/decline """
     mailing_item = Document.objects.get(id=instance_id)
     subject = f'Doc {mailing_item.name} is {STATUS_CHOICES[mailing_item.status]}'
     message = f'Dear {mailing_item.owner.email}, your doc {mailing_item.name} is {STATUS_CHOICES[mailing_item.status]}'
     email = [mailing_item.owner.email]
+    print(email)
     send_mail(
         subject,
         message,
