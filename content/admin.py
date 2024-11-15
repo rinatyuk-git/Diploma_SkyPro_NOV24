5cf9e4ce344110aaebc319697d4c80dfb96443cf
rinatyuk-git <17_ufo_RB@proton.me>
2024-11-15 21:01:56 +0100

    cleanup code under flake hints

--- content/admin.py
+++ content/admin.py
@@ -6,16 +6,18 @@ from content.tasks import doc_approving_message
 
 @admin.action(description="Mark selected docs as approved")
 def make_approved(modeladmin, request, queryset):
+    """ Marking docs as approved """
     queryset.update(status="a")
-    """ определение отправки сообщения """
+    """ Sending message to Owner regard approving """
     for obj in queryset:
         doc_approving_message.delay(obj.id)
 
 
 @admin.action(description="Mark selected docs as declined")
 def make_declined(modeladmin, request, queryset):
+    """ Marking docs as declined """
     queryset.update(status="d")
-    """ определение отправки сообщения """
+    """ Sending message to Owner regard decline """
     for obj in queryset:
         doc_approving_message.delay(obj.id)
 
